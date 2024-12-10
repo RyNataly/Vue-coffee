40f1abc7596e2a45f100c76d1fa1ad8357d08894
Natalia Ryzhenko <ryzhena@gmail.com>
2024-12-10 17:36:32 +0300

    Объект дата

--- src/views/ForYourPleasure.vue
+++ src/views/ForYourPleasure.vue
@@ -57,5 +57,47 @@ import ProductCard from '@/components/ProductCard.vue'
 
 export default {
    components: {NavBarComponent, ProductCard},
+    data() {
+    return {
+      product: [
+        {
+          id: 0,
+          img: 'coffee-3.jpg',
+          title: 'Solimo Coffee Beans 2kg',
+          price: '10.73$'
+        },
+        {
+          id: 1,
+          img: 'coffee-3.jpg',
+          title: 'Presto Coffee Beans 1kg',
+          price: '15.99$'
+        },
+        {
+          id: 2,
+          img: 'coffee-3.jpg',
+          title: 'AROMISTICO Coffee 1kg',
+          price: '6.99$'
+        },
+        {
+          id: 3,
+          img: 'coffee-3.jpg',
+          title: 'Solimo Coffee Beans 5kg',
+          price: '11.73$'
+        },
+        {
+          id: 4,
+          img: 'coffee-3.jpg',
+          title: 'Presto Coffee Beans 5kg',
+          price: '16.99$'
+        },
+        {
+          id: 5,
+          img: 'coffee-3.jpg',
+          title: 'AROMISTICO Coffee 5kg',
+          price: '7.99$'
+        }
+      ]
+    }
+  }
 }
 </script>
